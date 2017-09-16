035a18ed3291624a97931031a8d5de197dd9d193
Brittany Lacoste <lacostebrittany0227@gmail.com>
2017-09-15 18:26:56 -0700

    3rd test pass

--- src/Cards.js
+++ src/Cards.js
@@ -7,6 +7,9 @@ Cards.prototype.whoWins = (pack1, pack2) => {
     else if (pack1[0] > pack2[0]) {
         return "Player 1 wins 1 to 0";
     }
+    else {
+        return "Tie";
+    }
 }
 
 module.exports = new Cards();
