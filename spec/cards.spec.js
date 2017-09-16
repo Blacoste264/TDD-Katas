6ddd52e2ea98d290bee62b3739e66987b36e2d76
Brittany LaCoste <lacostebrittany0227@gmail.com>
2017-09-15 19:08:48 -0700

    Update cards.spec.js

--- spec/cards.spec.js
+++ spec/cards.spec.js
@@ -1,6 +1,6 @@
 const cards = require("../src/Cards");
-const cardRank = "123"
 
+// Tests cards game class
 describe("Cards game", () => {
     it("Wins with the highest card in a single game", () => {
         expect(cards.whoWins(["1"], ["2"])).toEqual("Player 2 wins 1 to 0");
