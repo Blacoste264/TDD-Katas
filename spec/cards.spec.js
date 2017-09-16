ff6c98b159396be44911eb197dc7b9175701d013
Brittany Lacoste <lacostebrittany0227@gmail.com>
2017-09-15 18:24:27 -0700

    2nd test passed

--- spec/cards.spec.js
+++ spec/cards.spec.js
@@ -1,7 +1,11 @@
 const cards = require("../src/Cards");
+const cardRank = "123"
 
 describe("Cards game", () => {
-    it("Winds with the highest card in a single game", () => {
+    it("Wins with the highest card in a single game", () => {
         expect(cards.whoWins(["1"], ["2"])).toEqual("Player 2 wins 1 to 0");
+    })
+    it("Wins with the highest card in the other pack in a single game", () => {
+        expect(cards.whoWins(["2"], ["1"])).toEqual("Player 1 wins 1 to 0");
     })
 })
--- src/Cards.js
+++ src/Cards.js
@@ -1,7 +1,12 @@
 const Cards = function () {};
 
 Cards.prototype.whoWins = (pack1, pack2) => {
-    return "Player 2 wins 1 to 0";
+    if (pack1[0] < pack2[0]) {
+        return "Player 2 wins 1 to 0";
+    }
+    else if (pack1[0] > pack2[0]) {
+        return "Player 1 wins 1 to 0";
+    }
 }
 
 module.exports = new Cards();
