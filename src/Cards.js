1742d4cd7500713ef54c3e915e2527ce9d650d8b
Brittany LaCoste <lacostebrittany0227@gmail.com>
2017-09-15 19:08:07 -0700

    Update Cards.js

--- src/Cards.js
+++ src/Cards.js
@@ -1,15 +1,17 @@
+// Constructor for Cards class
 const Cards = function () {};
 
+// Create new method whoWins for Cards class
 Cards.prototype.whoWins = (pack1, pack2) => {
     const cardRank = "123456789TJQK"
     let scorePlayer1 = 0,
         scorePlayer2 = 0,
         i;
 
-    calculateScore();
-    return humanReadableScore();
+    calculateScore(); // Call calculateScore first
+    return humanReadableScore(); // Take calculateScore's result, then plug into humanReadableScore for string output
 
-    function calculateScore() {
+    function calculateScore() { // Compare card values of pack1 to pack2 and add one to score if higher than pack2
         for (i = 0; i < pack1.length; ++i) {
             if (cardRank.indexOf(pack1[i]) < cardRank.indexOf(pack2[i])) {
                 ++scorePlayer2;
@@ -18,7 +20,7 @@ Cards.prototype.whoWins = (pack1, pack2) => {
             }
         }
 
-        if (pack1.length > 1) {
+        if (pack1.length > 1) { // If the pack1 has more than one card, continue to compare
             if (cardRank.indexOf(pack1[1]) < cardRank.indexOf(pack2[1])) {
                 ++scorePlayer2;
             } else if (cardRank.indexOf(pack1[1]) > cardRank.indexOf(pack2[1])) {
@@ -27,7 +29,7 @@ Cards.prototype.whoWins = (pack1, pack2) => {
         };
     }
 
-    function humanReadableScore() {
+    function humanReadableScore() { // Provides string version of results so humans can read O_O
         if (scorePlayer2 > scorePlayer1) {
             return "Player 2 wins 1 to 0";
         } else if (scorePlayer1 > scorePlayer2) {
@@ -39,5 +41,5 @@ Cards.prototype.whoWins = (pack1, pack2) => {
 }
 
 
-
+// Export new instance of Cards class
 module.exports = new Cards();
