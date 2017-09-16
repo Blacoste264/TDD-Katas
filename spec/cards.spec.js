82e0a2bdd91dc681952a9fc120a378938c3750c6
Brittany Lacoste <lacostebrittany0227@gmail.com>
2017-09-15 18:55:18 -0700

    Tests pass for cards, and refactored

--- spec/cards.spec.js
+++ spec/cards.spec.js
@@ -9,6 +9,12 @@ describe("Cards game", () => {
         expect(cards.whoWins(["2"], ["1"])).toEqual("Player 1 wins 1 to 0");
     })
     it("Ties", () => {
-        expect(whoWins(["1"], ["1"])).toEqual("Tie");
+        expect(cards.whoWins(["1"], ["1"])).toEqual("Tie");
+    })
+    it("Knows the card ranking which not always matches the correct order", () => {
+        expect(cards.whoWins(["K"], ["Q"])).toEqual("Player 1 wins 1 to 0");
+    })
+    it("Wins with several cards", () => {
+        expect(cards.whoWins(["1", "2"], ["1", "1"])).toEqual("Player 1 wins 1 to 0");
     })
 })
--- src/Cards.js
+++ src/Cards.js
@@ -1,15 +1,43 @@
 const Cards = function () {};
 
 Cards.prototype.whoWins = (pack1, pack2) => {
-    if (pack1[0] < pack2[0]) {
-        return "Player 2 wins 1 to 0";
+    const cardRank = "123456789TJQK"
+    let scorePlayer1 = 0,
+        scorePlayer2 = 0,
+        i;
+
+    calculateScore();
+    return humanReadableScore();
+
+    function calculateScore() {
+        for (i = 0; i < pack1.length; ++i) {
+            if (cardRank.indexOf(pack1[i]) < cardRank.indexOf(pack2[i])) {
+                ++scorePlayer2;
+            } else if (cardRank.indexOf(pack1[i]) > cardRank.indexOf(pack2[i])) {
+                ++scorePlayer1;
+            }
+        }
+
+        if (pack1.length > 1) {
+            if (cardRank.indexOf(pack1[1]) < cardRank.indexOf(pack2[1])) {
+                ++scorePlayer2;
+            } else if (cardRank.indexOf(pack1[1]) > cardRank.indexOf(pack2[1])) {
+                ++scorePlayer1;
+            }
+        };
     }
-    else if (pack1[0] > pack2[0]) {
-        return "Player 1 wins 1 to 0";
-    }
-    else {
-        return "Tie";
-    }
+
+    function humanReadableScore() {
+        if (scorePlayer2 > scorePlayer1) {
+            return "Player 2 wins 1 to 0";
+        } else if (scorePlayer1 > scorePlayer2) {
+            return "Player 1 wins 1 to 0";
+        } else {
+            return "Tie";
+        }
+    };
 }
+
+
 
 module.exports = new Cards();
