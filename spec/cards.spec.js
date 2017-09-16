b688e1d48bee14f46041515c8d7b5605fe3b15b5
Brittany Lacoste <lacostebrittany0227@gmail.com>
2017-09-15 18:25:55 -0700

    3rd test fail

--- spec/cards.spec.js
+++ spec/cards.spec.js
@@ -8,4 +8,7 @@ describe("Cards game", () => {
     it("Wins with the highest card in the other pack in a single game", () => {
         expect(cards.whoWins(["2"], ["1"])).toEqual("Player 1 wins 1 to 0");
     })
+    it("Ties", () => {
+        expect(whoWins(["1"], ["1"])).toEqual("Tie");
+    })
 })
